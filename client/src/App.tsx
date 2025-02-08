838258f504810f59c44b1e857ebf5a3ff35e1482
daljit singh <diljit2164@gmail.com>
2025-02-08 17:25:13 +0530

    propmpt Page created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
 
 
 import './App.css'
+import PromptPage from './pages/Main/PromptPage'
 
 function App() {
 
 
   return (
-    <>
+    
+    <><div className=" mt-0 bg-black" style={{width:"100vw",height:"100vh"}}>
+  <PromptPage />
+    </div>
   
     </>
+ 
+    
   )
 }
 
