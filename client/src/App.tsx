2d3e7fa52f373ca2ff158488bafc52d7a15be4da
CodeSciRahul <rahulkumarkudra2004@gmail.com>
2025-02-08 23:51:42 +0530

    Bug: resolve bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
     <Routes>
       <Route path='login' element={<Login />} />
       <Route path='signup' element={<Register />} />
+      <Route path='/' element={<Dashboard />}></Route>
     </Routes>
     </Router>
     </>
