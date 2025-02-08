f57a3f156f5a167a1ed8be31c5835d76f558831b
daljit singh <diljit2164@gmail.com>
2025-02-08 19:47:44 +0530

    this is save project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Login from './pages/auth/Login'
 import Register from './pages/auth/register'
-
+import Dashboard from './pages/Dashboard/Dashbaord'
 function App() {
 
 
   return (
     <>
+
     <Router basename='/'>
     <Routes>
       <Route path='login' element={<Login />} />
